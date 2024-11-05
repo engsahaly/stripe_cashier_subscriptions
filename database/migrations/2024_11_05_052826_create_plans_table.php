f37018ee6e4d1ae99e9dd290a5a079feee14c3e2
engsahaly <engsahaly@gmail.com>
2024-11-05 07:58:02 +0200

    add price field in plans migration file

--- database/migrations/2024_11_05_052826_create_plans_table.php
+++ database/migrations/2024_11_05_052826_create_plans_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('plans', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
+            $table->integer('price');
             $table->string('stripe_price_id');
             $table->timestamps();
         });
