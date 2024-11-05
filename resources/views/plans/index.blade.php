6e75146a7be588b269b2f063c2ba7084391d56e2
engsahaly <engsahaly@gmail.com>
2024-11-05 07:43:53 +0200

    plans design page

--- resources/views/plans/index.blade.php
+++ resources/views/plans/index.blade.php
@@ -9,7 +9,30 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    Plans here
+
+                    <div class="d-flex justify-content-center flex-wrap gap-4">
+                        <div class="text-center flex-grow-1 pt-3 px-lg-4">
+                            <h3 class="fs-6 fw-semibold text-dark">Monthly</h3>
+                            <p class="mt-3 d-flex align-items-center justify-content-center">
+                                <span class="display-5 fw-bold text-dark">$10</span>
+                                <span class="ms-2 text-muted fs-6">/month</span>
+                            </p>
+                            <a href="#" class="btn btn-primary mt-3 w-50">
+                                Buy plan
+                            </a>
+                        </div>
+                        <div class="text-center flex-grow-1 pt-3 px-lg-4">
+                            <h3 class="fs-6 fw-semibold text-dark">Yearly</h3>
+                            <p class="mt-3 d-flex align-items-center justify-content-center">
+                                <span class="display-5 fw-bold text-dark">$120</span>
+                                <span class="ms-2 text-muted fs-6">/year</span>
+                            </p>
+                            <a href="#" class="btn btn-primary mt-3 w-50">
+                                Buy plan
+                            </a>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
